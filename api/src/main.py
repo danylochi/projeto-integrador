89e2ae0542edae29bba38a5ef33e83544ef5f8a1
Dayane Domingues <65088681+danylochi@users.noreply.github.com>
2021-10-31 01:33:50 -0300

    rotina de inclusão no banco

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,13 @@
 from typing import Optional
 from fastapi import FastAPI, File, UploadFile
-import pandas as pd
-import xlrd
+import pandas
+import mysql.connector
+
+conexion = mysql.connector.connect(user='root', password='root',
+                              host='127.0.0.1',
+                              database='educacaodb')
+#conexion.close()
+
 
 
 app = FastAPI()
@@ -16,11 +22,57 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
+
+
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    book = xlrd.open_workbook(file_contents=file.file.read())
-    df = pd.read_excel(book,engine='xlrd')
-    print(df.head())
-    return {"filename": file.filename}
+async def create_upload_file(content: UploadFile = File(...)):
+    
+    content_in_memory = content.file.read()
+
+    data_frame = pandas.read_excel(content_in_memory)
+
+    #conexion.open() 
+    query= conexion.cursor()
+    string_insert= ("INSERT INTO consulta_caed (ano, materia, turma, serie, bimestre, estudante, participacao, numero_itens_respondidos, "
+        " porcento_acertos, categoria_desempenho, tipo_intervencao, itens_acertados, "
+        " h_01, h_02, h_03, h_04, h_05, h_06, h_07, h_08, h_09, h_10, h_11, "
+        " h_12, h_13, h_14, h_15, h_16, h_17, h_18, h_19, h_20, h_21, h_22, "
+        " h_23, h_24, h_25, h_26, h_27, h_28, h_29, h_30) "
+        " VALUES (%(ano)s, %(materia)s,%(turma)s,%(serie)s,%(bimestre)s,%(estudante)s,%(participacao)s,%(numero_itens_respondidos)s,"
+        " %(porcento_acertos)s,%(categoria_desempenho)s,%(tipo_intervencao)s,%(itens_acertados)s,"
+        " %(h_01)s,%(h_02)s,%(h_03)s,%(h_04)s,%(h_05)s,%(h_06)s,%(h_07)s,%(h_08)s,%(h_09)s,%(h_10)s,%(h_11)s,"
+        " %(h_12)s,%(h_13)s,%(h_14)s,%(h_15)s,%(h_16)s,%(h_17)s,%(h_18)s,%(h_19)s,%(h_20)s,%(h_21)s,%(h_22)s,"
+        " %(h_23)s,%(h_24)s,%(h_25)s,%(h_26)s,%(h_27)s,%(h_28)s,%(h_29)s,%(h_30)s )")
+
+    print(string_insert)
+
+    for index, row in data_frame.iterrows(): 
+        data_aluno = {
+        'ano': row['ano'], 'materia': row['materia'], 'turma': row['turma'], 'serie': row['serie'], 'bimestre': row['Bimestre'], 
+        'estudante': row['ESTUDANTE'], 'participacao': row['PARTICIPAÇÃO'], 'numero_itens_respondidos': row['Nº DE ITENS RESPONDIDOS'], 
+        'porcento_acertos': row['% ACERTOS'], 'categoria_desempenho': row['CATEGORIA DE DESEMPENHO'], 
+        'tipo_intervencao': row['TIPO DE INTERVENÇÃO'], 'itens_acertados': row['ITENS ACERTADOS'], 
+        'h_01': row['H 01'], 'h_02': row['H 02'], 'h_03': row['H 03'], 'h_04': row['H 04'], 'h_05': row['H 05'], 'h_06': row['H 06'], 
+        'h_07': row['H 07'], 
+        'h_08': row['H 08'], 'h_09': row['H 09'], 'h_10': row['H 10'], 'h_11': row['H 11'], 'h_12': row['H 12'], 'h_13': row['H 13'], 
+        'h_14': row['H 14'], 
+        'h_15': row['H 15'], 'h_16': row['H 16'], 'h_17': row['H 17'], 'h_18': row['H 18'], 'h_19': row['H 19'], 'h_20': row['H 20'], 
+        'h_21': row['H 21'], 
+        'h_22': row['H 22'], 'h_23': row['H 23'], 'h_24': row['H 24'], 'h_25': row['H 25'], 'h_26': row['H 26'], 'h_27': row['H 27'], 
+        'h_28': row['H 28'], 
+        'h_29': row['H 29'], 'h_30': row['H 30']
+        }    
+
+        query.execute(string_insert, data_aluno)
+
+    conexion.commit()
+    query.close()
+    #conexion.close()
+
+
+    
+
+
+    return {"filename": content.filename}
 
 
