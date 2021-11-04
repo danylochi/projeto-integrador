6118fb3bfb13cd643990ab3734c57f3448209088
Dayane Domingues <65088681+danylochi@users.noreply.github.com>
2021-11-04 01:34:17 -0300

    fiz insert tabela habilidade

--- api/src/main.py
+++ api/src/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI, File, UploadFile
 import pandas
 import mysql.connector
 
+#cria conexão com o banco de dados mysql
 conexion = mysql.connector.connect(user='root', password='root',
                               host='localhost',
                               database='educacaodb')
@@ -27,12 +28,19 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.post("/uploadfile/")
 async def create_upload_file(content: UploadFile = File(...)):
     
+    #ler o arquivo e armazenar o conteúdo na memória 
     content_in_memory = content.file.read()
 
+    #ler conteúdo da memória e converter para data frame 
     data_frame = pandas.read_excel(content_in_memory)
 
     #conexion.open() 
+
+    #---------------------------------COMANDOS TABELA CONSULTA CAED------------------------------------------------------
+    # prepara query de insert 
     query_insert= conexion.cursor()
+
+    #cria string com comando de insert 
     string_insert= ("INSERT INTO consulta_caed (ano, materia, turma, serie, bimestre, estudante, participacao, numero_itens_respondidos, "
         " porcento_acertos, categoria_desempenho, tipo_intervencao, itens_acertados, "
         " h_01, h_02, h_03, h_04, h_05, h_06, h_07, h_08, h_09, h_10, h_11, "
@@ -44,7 +52,10 @@ async def create_upload_file(content: UploadFile = File(...)):
         " %(h_12)s,%(h_13)s,%(h_14)s,%(h_15)s,%(h_16)s,%(h_17)s,%(h_18)s,%(h_19)s,%(h_20)s,%(h_21)s,%(h_22)s,"
         " %(h_23)s,%(h_24)s,%(h_25)s,%(h_26)s,%(h_27)s,%(h_28)s,%(h_29)s,%(h_30)s )")
 
+    #prepara query de consulta 
     query_consulta=conexion.cursor()
+
+    #cria string com comando de consulta 
     string_consulta= ("SELECT * FROM educacaodb.consulta_caed "
         " WHERE ano = %(ano)s"
         " AND materia = %(materia)s"
@@ -53,7 +64,10 @@ async def create_upload_file(content: UploadFile = File(...)):
         " AND bimestre = %(bimestre)s" 
         " AND estudante = %(estudante)s") 
 
+    #prepara query de update 
     query_update=conexion.cursor()
+
+    #cria string com comando de update 
     string_update= ("UPDATE consulta_caed SET participacao = %(participacao)s, numero_itens_respondidos = %(numero_itens_respondidos)s, "
          " porcento_acertos = %(porcento_acertos)s, categoria_desempenho = %(categoria_desempenho)s, "
          " tipo_intervencao = %(tipo_intervencao)s, itens_acertados = %(itens_acertados)s, "
@@ -68,8 +82,36 @@ async def create_upload_file(content: UploadFile = File(...)):
          " AND serie = %(serie)s"
          " AND bimestre = %(bimestre)s" 
          " AND estudante = %(estudante)s") 
+    #-----------------------------------------------------------------------------------------------------     
 
+    #---------------------------------COMANDOS TABELA HABILIDADE CAED------------------------------------------------------
+    # prepara query de insert 
+    query_insert_hab= conexion.cursor()
+
+    #cria string com comando de insert 
+    string_insert_hab= ("INSERT INTO habilidade_caed (ano, materia, turma, serie, bimestre, questao) "
+        " VALUES (%(ano)s, %(materia)s,%(turma)s,%(serie)s,%(bimestre)s,%(questao)s )")
+
+    #prepara query de consulta 
+    query_consulta_hab=conexion.cursor()
+
+    #cria string com comando de consulta 
+    string_consulta_hab= ("SELECT * FROM educacaodb.habilidade_caed "
+        " WHERE ano = %(ano)s"
+        " AND materia = %(materia)s"
+        " AND turma = %(turma)s"
+        " AND serie = %(serie)s"
+        " AND bimestre = %(bimestre)s" 
+        " AND questao = %(questao)s")  
+
+    #-----------------------------------------------------------------------------------------------------     
+    
+    
+    #---------------------------------EXECUÇÃO TABELA CONSULTA CAED------------------------------------------------------
+    #percorre planilha linha a linha através do data frame 
     for index, row in data_frame.iterrows(): 
+
+        #cria dados de aluno com o conteúdo da linha para insert/update 
         data_aluno = {
         'ano': row['ano'], 'materia': row['materia'], 'turma': row['turma'], 'serie': row['serie'], 'bimestre': row['Bimestre'], 
         'estudante': row['ESTUDANTE'], 'participacao': row['PARTICIPAÇÃO'], 'numero_itens_respondidos': row['Nº DE ITENS RESPONDIDOS'], 
@@ -86,25 +128,67 @@ async def create_upload_file(content: UploadFile = File(...)):
         'h_29': row['H 29'], 'h_30': row['H 30']
         } 
 
+        #cria dados de aluno com o conteúdo da linha para consulta  
         data_consulta = {
         'ano': row['ano'], 'materia': row['materia'], 'turma': row['turma'], 'serie': row['serie'], 'bimestre': row['Bimestre'], 
         'estudante': row['ESTUDANTE']
         }
-           
+        
+        #executa a consulta   
         query_consulta.execute(string_consulta, data_consulta)
+
+        #força carregar todos os dados da consulta, ignora modo lazy 
         query_consulta.fetchall()
 
+        #verifica se linha já foi inserida 
         if query_consulta.rowcount > 0:
+            #se rowcount>0 significa que linha já foi inserida e devemos atualizá-la 
             query_update.execute(string_update, data_aluno)
             query_update.fetchall()
         else:
+            #se rowcount=0 significa que linha não foi inserida e devemos inseri-la 
             query_insert.execute(string_insert, data_aluno)
             query_insert.fetchall()
+    #-----------------------------------------------------------------------------------------------------     
 
+    #---------------------------------EXECUÇÃO TABELA HABILIDADE CAED------------------------------------------------------
+        
+    row_one=next(data_frame.iterrows())[1]
+
+    print(row_one)
+
+    #percorre planilha linha a linha através do data frame 
+    for i in range(1,31):
+
+        #cria dados de aluno com o conteúdo da linha para insert/update 
+        data_hab = {
+            'ano': row_one['ano'], 'materia': row_one['materia'], 'turma': row_one['turma'], 'serie': row_one['serie'], 'bimestre': row_one['Bimestre'], 
+            'questao': 'h_'+f'{i:02}'
+        }
+
+        #executa a consulta   
+        query_consulta_hab.execute(string_consulta_hab, data_hab)
+
+        #força carregar todos os dados da consulta, ignora modo lazy 
+        query_consulta_hab.fetchall()
+
+        #verifica se linha já foi inserida 
+        if query_consulta_hab.rowcount == 0:
+            #se rowcount=0 significa que linha não foi inserida e devemos inseri-la 
+            query_insert_hab.execute(string_insert_hab, data_hab)
+            query_insert_hab.fetchall()
+
+    #-----------------------------------------------------------------------------------------------------            
+
+    #efetiva todas as transações no banco 
     conexion.commit()
+
+    #fecha as querys
     query_insert.close()
     query_consulta.close()
     query_update.close()
+    query_insert_hab.close()
+    query_consulta_hab.close()    
 
     #conexion.close()
 
